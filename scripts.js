5367edf3aa3840548e634cc5aa5df765302f2ae6
Casey The Great <casey@caseypitman.com>
2020-05-11 13:46:07 -0500

    added new background and quotes

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 
 let time =new Date();    //retrieve time
-let timeAbbr = time.getHours()>12 ? "p.m." : "a.m";   //set time abbreviation
+let timeAbbr = time.getHours()>11 ? "p.m." : "a.m";   //set time abbreviation
 let hours = time.getHours()>12 ? time.getHours() - 12 : time.getHours(); //no military time
 let minutes = time.getMinutes(); 
 if (minutes<10){                        //add zero before single digit minutes
@@ -10,23 +10,41 @@ if (minutes<10){                        //add zero before single digit minutes
 let displayTime = hours + ":" + minutes + " "+ timeAbbr;    //put the time together
 $("#time").text(displayTime);       //display time on DOM
 
-if (time.getHours()<20 && time.getHours()>7){      // background color based on time of day 
-    //day time hours
-    $("#weather-container").css("background", "linear-gradient(180deg, rgba(37,27,215,1) 0%, rgba(65,188,236,0.95) 60%)")
-}else{
-    //night time hours
-    $("#weather-container").css("background", "linear-gradient(180deg, rgba(3,2,22,1) 0%, rgba(9,9,121,0.95) 50%, rgba(14,111,130,1) 100%)")
-}
+// GALACTUS QUOTES - 19 items//
+let quote = ['"My Herald, the great hunger is upon me. Where is the next world upon which I can feed?"',
+             "My journey is ended! This planet shall sustain me until it has been drained of all elemental life! So speaks Galactus!",
+             "I HUNGER!",
+             "I have come from the farthest reaches of space &mdash; to proclaim the doom of the human race!",
+             "I have need of a new Herald...",
+             "I am power incarnate! To Galactus, nothing is impossible!",
+             "Galactus does what Galactus must to survive.",
+             "Galactus... Hungers.",
+             "I am inevitable. I am... Destiny. I am Galactus and I hunger.",
+             "Soon I will outgrow this form... and I grow... hungry.",
+             '"No living thing dares ignore my call. For I am that which has always been, since this Universe was birthed from the ashes of the last."',
+             "I am the hunger that dooms worlds.",
+             "I am he who is beyond such frail Concepts as good and evil. I am Galactus.",
+             "These beings die for the highest of callings -- to keep me alive. Such is the way of things.",
+             "Human suffering is irrelevant to me!",
+             "Who are you? Who is so ignorant as to confront Galactus in his sanctum?",
+             "I know not who or what they are below! I only know Galactus must feed!",
+             "I am Galactus. The be-all and end-all am I!",
+             "You exist solely by the grace I have given and the leash you tug against. Or have you forgotten whom you serve, Little God?!"
+];
+
+$("#quote").text(quote[10])
+             
+
 //CALL FOR WEATHER CONDITIONS
 $.getJSON(
     "https://api.openweathermap.org/data/2.5/weather?q=texarkana&units=imperial&appid=610c6bd442f660648855be9d7080adc6",
     function(data){
         
-        let icon = "https://openweathermap.org/img/wn/" + data.weather[0].icon +".png"; //get icon
+       // let icon = "https://openweathermap.org/img/wn/" + data.weather[0].icon +".png"; //get icon
         let temp = Math.round(data.main.temp);  //get temp
         let weather = data.weather[0].main; //get weather condition
         //Display weather on DOM
-        $("#icon").attr("src", icon);
+        // $("#icon").attr("src", icon);
         $("#temp").append(temp + " &deg;");
         $("#weather").append(weather);
 })
