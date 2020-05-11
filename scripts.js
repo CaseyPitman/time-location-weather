4f539be0b11bf98b7e098da04625e61959e22e78
Casey The Great <casey@caseypitman.com>
2020-05-11 14:52:24 -0500

    ship

--- scripts.js
+++ scripts.js
@@ -11,28 +11,30 @@ let displayTime = hours + ":" + minutes + " "+ timeAbbr;    //put the time toget
 $("#time").text(displayTime);       //display time on DOM
 
 // GALACTUS QUOTES - 19 items//
-let quote = ['"My Herald, the great hunger is upon me. Where is the next world upon which I can feed?"',
-             "My journey is ended! This planet shall sustain me until it has been drained of all elemental life! So speaks Galactus!",
-             "I HUNGER!",
-             "I have come from the farthest reaches of space &mdash; to proclaim the doom of the human race!",
-             "I have need of a new Herald...",
-             "I am power incarnate! To Galactus, nothing is impossible!",
-             "Galactus does what Galactus must to survive.",
-             "Galactus... Hungers.",
-             "I am inevitable. I am... Destiny. I am Galactus and I hunger.",
-             "Soon I will outgrow this form... and I grow... hungry.",
-             '"No living thing dares ignore my call. For I am that which has always been, since this Universe was birthed from the ashes of the last."',
-             "I am the hunger that dooms worlds.",
-             "I am he who is beyond such frail Concepts as good and evil. I am Galactus.",
-             "These beings die for the highest of callings -- to keep me alive. Such is the way of things.",
-             "Human suffering is irrelevant to me!",
-             "Who are you? Who is so ignorant as to confront Galactus in his sanctum?",
-             "I know not who or what they are below! I only know Galactus must feed!",
-             "I am Galactus. The be-all and end-all am I!",
-             "You exist solely by the grace I have given and the leash you tug against. Or have you forgotten whom you serve, Little God?!"
-];
+let quote = ['\"My Herald, the great hunger is upon me. Where is the next world upon which I can feed?\"',
+             '\"My journey is ended! This planet shall sustain me until it has been drained of all elemental life! So speaks Galactus!\"',
+             '\"I HUNGER!\"',
+             '\"I have come from the farthest reaches of space &mdash; to proclaim the doom of the human race!\"',
+             '\"I have need of a new Herald...\"',
+             '\"I am power incarnate! To Galactus, nothing is impossible!\"',
+             '\"Galactus does what Galactus must to survive.\"',
+             '\"Galactus...Hungers.\"',
+             '\"I am inevitable. I am... Destiny. I am Galactus and I hunger.\"',
+             '\"Soon I will outgrow this form...and I grow...hungry.\"',
+             '\"No living thing dares ignore my call. For I am that which has always been, since this Universe was birthed from the ashes of the last.\"',
+             '\"I am the hunger that dooms worlds.\"',
+             '\"I am he who is beyond such frail Concepts as good and evil. I am Galactus.\"',
+             '\"These beings die for the highest of callings\u2014to keep me alive. Such is the way of things.\"',
+             '\"Human suffering is irrelevant to me!\"',
+             '\"Who are you? Who is so ignorant as to confront Galactus in his sanctum?\"',
+             '\"I know not who or what they are below! I only know Galactus must feed!\"',
+             '\"I am Galactus. The be-all and end-all am I!\"',
+             '\"You exist solely by the grace I have given and the leash you tug against. Or have you forgotten whom you serve, Little God?\"'
+]
 
-$("#quote").text(quote[10])
+let quoteNumb = Math.floor(Math.random() * 20);
+
+$("#quote").text(quote[quoteNumb]);
              
 
 //CALL FOR WEATHER CONDITIONS
